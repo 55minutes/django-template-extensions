b635097787858d999dda84578ba39eb0d89ab500
gsong <george@55minutes.com>
2007-06-04 01:07:23 +0000

    * Multiply returns 0 is something goes wrong
    git-svn-id: file:///opt/custom/var/svn/repos/internal/55minutes/django_projects/fiftyfive/trunk/apps/customtags@5827 07aa3797-eaea-0310-9497-ec20f46c85ba

--- templatetags/fiftyfive_filters.py
+++ templatetags/fiftyfive_filters.py
@@ -5,13 +5,16 @@ def tabularize(value, cols):
     """modifies a list to become a list of lists
     eg [1,2,3,4] becomes [[1,2], [3,4]] with an argument of 2"""
     try:
-            cols = int(cols)
+        cols = int(cols)
     except ValueError:
-            return [value]
+        return [value]
     return map(*([None] + [value[i::cols] for i in range(0, cols)]))
 register.filter('tabularize', tabularize)
 
 def multiply(value, number):
     """Multiplies the value by numer"""
-    return float(value)*number
+    try:
+        return float(value)*number
+    except:
+        return 0
 register.filter('multiply', multiply)
