3ef4d2d34ce5e931a8c9d2053f3c9633c89c1b94
gsong <george@55minutes.com>
2009-12-31 07:56:56 +0000

    Added truncate and smart_if tags.
    git-svn-id: file:///opt/custom/var/svn/repos/internal/fiftyfive/python/trunk/django/apps/55-template-extensions@8873 07aa3797-eaea-0310-9497-ec20f46c85ba

---
+++ src/fiftyfive_template_extensions/templatetags/ff_truncate.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from django import template
+
+register = template.Library()
+
+@register.filter
+# truncate after a certain number of characters
+def truncchar(value, arg):
+    if len(value) < arg:
+        return value
+    else:
+        return value[:arg] + u'…'
